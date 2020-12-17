416c38a36fa71ad2c3accc644fd994663845f0a0
theHimanshuShekhar <hshekhar.dev@gmail.com>
2020-12-18 03:25:26 +0530

    add ftb port

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
-
-
 const ngrok = require('ngrok');
 const mc_port = process.env.MC_PORT || 25565
+const ftb_port = process.env.FTB_PORT || 25566
 let url
+let ftb_url
 
 const ngrok_runner = () => {
     if (!url) {
@@ -24,9 +24,25 @@ const ngrok_runner = () => {
             authtoken: '1lBvXaBvawLrBI39dJ3fW16y4Od_4sYJhCoQbaunLjVcwjgWE'
         })
         .then(url => {
-            console.log('ip: '+ url);
+            console.log('Vanilla ip: '+ url);
             db.collection('bhayanak').doc('minecraft-server').set({
                 ip: url
+            });
+        })
+        .catch((reason) => console.info(reason));
+    }
+
+    if(!ftb_url) {
+        ngrok.connect({
+            proto: 'tcp',
+            addr: ftb_port,
+            region: 'in',
+            authtoken: '1lBvXaBvawLrBI39dJ3fW16y4Od_4sYJhCoQbaunLjVcwjgWE'
+        })
+        .then(url => {
+            console.log('FTB ip: '+ url);
+            db.collection('bhayanak').doc('ftb-server').set({
+                ip: ftb_url
             });
         })
         .catch((reason) => console.info(reason));
